f3d75e96983f9067541faa19805a475012f51b1d
Gary McWilliams <gary.mcwilliams@verint.com>
2017-10-04 10:40:44 +0100

    break teams.py into functions

--- icalendar/scratchpad/teams.py
+++ icalendar/scratchpad/teams.py
@@ -6,37 +6,46 @@ from icalendar import vDatetime, Calendar, Event
 import pytz, os
 from tempfile import gettempdir
 
-with open('teams.json') as data_file:    
-    json_data = json.load(data_file)
-print(json.dumps(json_data, indent=2))
+def load_json( json_filename ):
+    "loads the JSON file"
+    with open(json_filename) as data_file:    
+        json_data = json.load(data_file)
+    #print(json.dumps(json_teamdata, indent=2))
+    return json_data;
 
-#print json_data['data']
-print(json_data['teams'])
-# {'251228454889939/insights/page_fan_adds_unique/day': ...
+def create_event( event_data ):
+    """Create an event for the given event_data"""
+    print(team['name'])
+    event = Event()
+    event['uid'] = '20050115T101010/27346262376@mxm.dk'
+    event['location'] = event_data['location']
+    event.add('priority', 5)
+
+    event.add('summary', 'Match against %s' % event_data['name'])
+    event.add('dtstart', datetime(2005, 4, 4, 8, 0, 0, tzinfo=pytz.utc))
+    event.add('dtend', datetime(2005, 4, 4, 10, 0, 0, tzinfo=pytz.utc))
+    event.add('dtstamp', datetime(2005, 4, 4, 0, 10, 0, tzinfo=pytz.utc))
+    
+    return event
+
+def mk_save_dir():    
+    newdir = os.path.join(gettempdir(), 'icalendar') 
+    if not os.path.exists(newdir):
+        os.makedirs(newdir)
+    return newdir
+
+
+json_teamdata = load_json('teams.json')
 
 cal = Calendar()
 # pre-reqs
 cal.add('prodid', '-//My calendar product//mxm.dk//')
 cal.add('version', '2.0')
 
-for team in json_data['teams']:
-    print(team['name'])
-    event = Event()
-    event['uid'] = '20050115T101010/27346262376@mxm.dk'
-    event['location'] = team['location']
-    event.add('priority', 5)
+for team in json_teamdata['teams']:
+    cal.add_component(create_event(team))
 
-    event.add('summary', 'Match against %s' % team['name'])
-    event.add('dtstart', datetime(2005, 4, 4, 8, 0, 0, tzinfo=pytz.utc))
-    event.add('dtend', datetime(2005, 4, 4, 10, 0, 0, tzinfo=pytz.utc))
-    event.add('dtstamp', datetime(2005, 4, 4, 0, 10, 0, tzinfo=pytz.utc))
-
-    cal.add_component(event)
-
-newdir = os.path.join(gettempdir(), 'icalendar') 
-newfile = os.path.join(newdir, 'falls.ics')
-if not os.path.exists(newdir):
-    os.makedirs(newdir)
+newfile = os.path.join(mk_save_dir(), 'falls.ics')
 f = open(newfile, 'wb')
 f.write(cal.to_ical())
 f.close()
