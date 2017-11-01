bad890291acd7e4f713cd2e6f6cf1c902095230c
gmcwilliams <gmcwilliams@10.0.75.1>
2017-11-01 11:09:12 +0000

    replaced individual team.py with a general main
    accept team/year on command line

---
+++ icalendar/ical/main.py
@@ -0,0 +1,19 @@
+from ical.events import Events
+
+import argparse
+from pathlib import Path
+import sys
+
+parser = argparse.ArgumentParser(description='Process Bowls matches.')
+parser.add_argument('-t', '--team')
+parser.add_argument('-y', '--year')
+
+args = parser.parse_args()
+checkFile = Path('%s_matches_%s.json' % (args.team, args.year))
+if (not checkFile.exists()):
+    print("Cannot find data for %s/%s" % (args.team, args.year))
+    sys.exit(1)
+
+events = Events(args.team, args.year)
+
+events.add_events()
--- icalendar/ical/stadiumfriday.py
+++
@@ -1,8 +0,0 @@
-from ical.events import Events
-
-year = '2017-18'
-
-events = Events('stadiumfriday', year)
-#events.set_savedir(gettempdir())
-
-events.add_events()
