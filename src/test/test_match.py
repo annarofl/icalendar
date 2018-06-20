b6c16e324647cf814998a6fbe5757cc93cbb0099
Gary McWilliams <gary.mcwilliams@verint.com>
2018-06-20 08:05:43 +0100

    added pytest fixture

--- src/test/test_match.py
+++ src/test/test_match.py
@@ -5,10 +5,10 @@ class TestMatch:
 
     @pytest.fixture(scope='class')
     def match(self) -> Match:
-        return Match("me", "home", 1, "away", 0, "2018-06-19", "location", 3)
+        return Match("me", "home", 1, "away", 0, "2018-06-19_14:00", "location", 3)
 
-    def test_description(self):
-        match = Match("me", "home", 1, "away", 0, "2018-06-19_14:00", "location", 3)
+    def test_description(self, match):
+#        match = Match("me", "home", 1, "away", 0, "2018-06-19_14:00", "location", 3)
         assert(match.description() == "home (1) v (0) away on 2018-06-19@14:00")
 
 #if __name__ == '__main__':
