560ca8b8da8f707bafb2c24285fd5b87788fbb4c
Gary McWilliams <gary@mc-williams.co.uk>
2018-07-06 08:20:06 +0100

    added (failing) envparse setup

--- src/main.py
+++ src/main.py
@@ -1,7 +1,17 @@
 from ical.events import Events
 
 import argparse
-from pathlib import Path
+
+from envparse import Env
+DATAPATH="gary"
+env = Env(
+    TEAM=str,
+    YEAR=str,
+    DATAPATH=str,
+)
+env.read_envfile()
+
+print(f"DATAPATH={DATAPATH}")
 
 parser = argparse.ArgumentParser(description="Process Bowls matches.")
 parser.add_argument("-t", "--team")
