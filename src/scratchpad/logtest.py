8eb910abadfe8aa99437c78db5874e735e9f564b
Gary McWilliams <gary.mcwilliams@verint.com>
2018-09-13 09:18:06 +0100

    #20:logging test refinements
    added setup_logging to allow ENV over-ride
    use yml format
    added pyyaml
    refine layout

--- src/scratchpad/logtest.py
+++ src/scratchpad/logtest.py
@@ -1,15 +1,37 @@
 import logging
 import logging.config
-#import yaml
-import json
+import os
+import yaml
+#import json
 
-def main():
-#    logging.config.dictConfig(yaml.load(open("logging.yml", "r")))
-    logging.config.dictConfig(json.load(open("logging.json", "r")))
+def setup_logging(
+    default_path='logging.yml',
+    default_level=logging.INFO,
+    env_key='LOG_CFG'
+):
+    """Setup logging configuration
+
+    """
+#    logging.config.fileConfig(fname='logging.conf', disable_existing_loggers=False)
+#    logging.config.dictConfig(json.load(open("logging.json", "r")))
 #    logging.basicConfig(filename="logs/main.log",level=logging.DEBUG,format='%(asctime)s: %(message)s')
 #    logging.basicConfig(filename="logs/main.log",level=logging.DEBUjsonrmat='%(asctime)s: %(message)s')
+
+    path = default_path
+    value = os.getenv(env_key, None)
+    if value:
+        path = value
+    if os.path.exists(path):
+        with open(path, 'rt') as f:
+            config = yaml.safe_load(f.read())
+        logging.config.dictConfig(config)
+    else:
+        logging.basicConfig(level=default_level)
+
+def main():
+    setup_logging()
  
-    logger = logging.getLogger("gary")
+    logger = logging.getLogger(__name__)
     logger.info(f"setting up Match")
 
 if __name__ == '__main__':
