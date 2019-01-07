62d5974a3ecf283cbe60bd834dbe3fc0877a0f2b
Gary McWilliams <gary@mc-williams.co.uk>
2019-01-07 08:29:58 +0000

    #33 docker: use envparse rather than os.environ

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from ical.events import Events
 
 import argparse
-import os
+from envparse import env
 
 parser = argparse.ArgumentParser(description="Process Bowls matches.")
 parser.add_argument("-t", "--team")
@@ -9,8 +9,8 @@ parser.add_argument("-y", "--year")
 
 args = parser.parse_args()
 
-team = args.team if args.team is not None else os.environ['ICAL_TEAM']
-year = args.year if args.year is not None else os.environ['ICAL_YEAR']
+team = args.team if args.team is not None else env('ICAL_TEAM')
+year = args.year if args.year is not None else env('ICAL_YEAR')
 
 events = Events(team, year)
 
